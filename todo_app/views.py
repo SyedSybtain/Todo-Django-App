ed7b1bc7d639c1999c27d14daf3d6079d6459223
SyedSybtain <sybtainsyed@gmail.com>
2024-12-04 16:54:35 +0500

    added tasks view of todo app

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Todo
+from .forms import task_form
 
-# Create your views here.
+def tasks(request):
+    html = 'todo_app/app.html'
+    tasks = Todo.objects.all()
+    form = task_form()
+    form_del = None
+    if 'del' in request.POST:
+        form_del = Todo.objects.get(id = request.POST['del'])
+    if request.method == 'POST':
+        if 'del' in request.POST:
+            form_del.delete()
+            return redirect('tasks')
+        if 'add' in request.POST:
+            form = task_form(request.POST)
+            form.save()
+            return redirect('tasks')
+
+    context = {'tasks':tasks,
+               'form':form,
+               'form_del':form_del
+               }
+    
+    return render(request,html,context)
